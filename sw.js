e72b0d5cdb4880075dc0fb9697f9158e63833cbd
alaouih22-cyber <alaouih22@gmail.com>
2026-03-04 23:30:10 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,36 +1,21 @@
-const CACHE_NAME = 'muslim-pro-v6';
-const ASSETS = [
-  'index.html',
-  'manifest.json',
-  'icon.png'
-];
+self.addEventListener('install', e => self.skipWaiting());
+self.addEventListener('activate', e => e.waitUntil(clients.claim()));
 
-self.addEventListener('install', (e) => {
-  e.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
-  );
-  self.skipWaiting();
+self.addEventListener('message', event => {
+    if (event.data && event.data.type === 'SHOW_PRAYER_BANNER') {
+        const options = {
+            body: event.data.body,
+            icon: 'icon.png',
+            badge: 'icon.png',
+            vibrate: [500, 110, 500],
+            requireInteraction: true,
+            tag: 'prayer-alert'
+        };
+        self.registration.showNotification(event.data.title, options);
+    }
 });
 
-self.addEventListener('activate', (e) => {
-  e.waitUntil(clients.claim());
-});
-
-self.addEventListener('fetch', (e) => {
-  e.respondWith(
-    caches.match(e.request).then((res) => res || fetch(e.request))
-  );
-});
-
-self.addEventListener('message', (event) => {
-  if (event.data && event.data.type === 'SHOW_PRAYER_BANNER') {
-    self.registration.showNotification(event.data.title, {
-      body: event.data.body,
-      icon: 'icon.png',
-      badge: 'icon.png',
-      vibrate: [500, 110, 500],
-      requireInteraction: true,
-      tag: 'prayer-alert'
-    });
-  }
+self.addEventListener('notificationclick', event => {
+    event.notification.close();
+    event.waitUntil(clients.openWindow('./'));
 });
